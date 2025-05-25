cf7b760c06a6b684db205acface3b8150a0e1c55
Ricard Ferrero <ricard.ferrero.dev@gmail.com>
2025-05-25 21:58:47 +0200

    Vectors WIP

---
+++ app/vector_space.rb
@@ -0,0 +1,70 @@
+[Integer, Float].each do |numerical|
+  numerical.class_eval do
+    alias_method :original_multiply, :*
+    def * n
+      return n * self if n.is_a? VectorSpace::Vector
+      original_multiply n
+    end
+  end
+end
+
+module VectorSpace
+  class Vector
+    attr_reader :coordenates
+
+    def initialize *args
+      self.coordenates = args
+    end
+
+    def dimension
+      coordenates.count
+    end
+
+    def + vector
+      raise "Cannot add #{vector.class} to vectors" unless vector.is_a? Vector
+      raise 'Cannot add vectors with different dimensions' unless dimension == vector.dimension
+      Vector.new *(0...dimension).map { |i| coordenates[i] + vector.coordenates[i] }
+    end
+
+    def - vector
+      raise "Cannot substract #{vector.class} to a vectors" unless vector.is_a? Vector
+      raise 'Cannot substract vectors with different dimensions' unless dimension == vector.dimension
+      Vector.new *(0...dimension).map { |i| coordenates[i] - vector.coordenates[i] }
+    end
+
+    def * scalar
+      raise 'Scalar must be Integer or Float' unless scalar.is_a? Numeric
+      Vector.new *coordenates.map { |c| c * scalar }
+    end
+
+    def == vector
+      case vector
+      when Vector
+        coordenates == vector.coordenates
+      when Array
+        coordenates == vector
+      else
+        false
+      end
+    end
+
+    def != vector
+      !(self==vector)
+    end
+
+    def coordenates= args
+      raise 'Coordenates must be defined within an Array' unless args.is_a? Array
+      raise 'Coordenates values must be Integer or Float' if args.map{|c| c.is_a? Numeric}.include? false
+      @coordenates = args
+    end
+
+    def to_s
+      "(#{coordenates.join(', ')})"
+    end
+
+    private
+      def inspect
+        "#<Vector #{self}>"
+      end
+  end
+end
