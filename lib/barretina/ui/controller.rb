42c2bae47535cecf6b7e2d435a70b9299c890ba0
Ricard Ferrero <ricard.ferrero.dev@gmail.com>
2025-10-06 16:08:30 +0200

    UI::Controller as a State Machine

--- lib/barretina/ui/controller.rb
+++ lib/barretina/ui/controller.rb
@@ -1,6 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'controller/state_label'
+
 module Barretina
   module UI
     class Controller
+      STATES = [:select, :create, :transform]
+      STATES.each do |state|
+        define_method("#{state}_state!") do
+          @state = state
+          @state_label.text = state
+        end
+        define_method("#{state}_state?") do
+          @state == state
+        end
+      end
+
+      def initialize
+        @state_label = StateLabel.new
+        select_state!
+      end
+
       def mouse event
         case event.type
         when :down
@@ -10,9 +30,24 @@ module Barretina
 
       def key event
         case event.type
+        when :down
+          key_down event.key
         when :held
+        when :up
         end
       end
+
+      private
+        def key_down event_key
+          case event_key
+          when 's'
+            select_state!
+          when 'c'
+            create_state!
+          when 't'
+            transform_state!
+          end
+        end
     end
   end
 end
